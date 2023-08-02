da875c2f2b703de1e990ff991b01ba61f51b85a9
Mohit Kalshan <mohit.kalshan@digimantra.com>
2023-08-02 18:29:19 +0530

    theme apply

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const btn = document.querySelector("#side-menu");
 const redbtn = document.querySelector(".red-color");
 const greenbtn = document.querySelector(".green-color");
 const yellowbtn = document.querySelector(".yellow-color");
+const greybtn = document.querySelector(".grey-color");
+
+//font variable
 
 // Change color theme
 const headerOverlay = document.querySelectorAll(".bg-overlay");
@@ -13,10 +16,13 @@ const colorChange = (color) => {
     ele.style.opacity = 0.08;
   })
 };
-
+// Color theme
 redbtn.addEventListener("click", () => colorChange("red"));
 greenbtn.addEventListener("click", () => colorChange("green"));
 yellowbtn.addEventListener("click", () => colorChange("yellow"));
+greybtn.addEventListener("click", () => colorChange("grey"));
+
+//Font theme
 
 // Toggle menu bar
 const toggleMenu = () => {
